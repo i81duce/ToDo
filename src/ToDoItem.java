a7f95113e7dc7c5108d53a0d4b154fcbeb137c3c
KevinBozic <bozic.kevin@gmail.com>
2016-02-08 11:49:54 -0500

    Write initial to-do app

---
+++ src/ToDoItem.java
@@ -0,0 +1,9 @@
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
